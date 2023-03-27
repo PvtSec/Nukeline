00011eb679e614af6d7d10b286cb4137f06c85fb
Akshay <akssofts@gmail.com>
2023-03-27 09:37:01 +0530

    Chore: init github actions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hey! Welcome to NukeLine!')
+  res.send('Hey!! Welcome to NukeLine!')
 })
 
 app.listen(3000, function () {
